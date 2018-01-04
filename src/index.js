225ba39ae922f9010d966fe2987d17c87e322bc5
Fritz Hoste <f.hoste@vmm.be>
2018-01-04 10:38:22 +0100

    Remove semicolons for PR

--- src/index.js
+++ src/index.js
@@ -13,74 +13,72 @@ class Ichimoku {
 		this.params = Object.assign({}, this.defaults, input);
 
 		this.generator = (function* () {
-			let result;
-			let tick;
+			let result
+			let tick
 
-			let period = Math.max(_this.params.conversionPeriod, _this.params.basePeriod, _this.params.spanPeriod, _this.params.displacement);
-			let periodCounter = 0;
-			let spanCounter = 0;
-			let lagCounter = 0;
-			let highs = [];
-			let lows = [];
-			let closes = [];
-			let spanAs = [];
-			let spanBs = [];
-			let lags = [];
+			let period = Math.max(_this.params.conversionPeriod, _this.params.basePeriod, _this.params.spanPeriod, _this.params.displacement)
+			let periodCounter = 0
+			let spanCounter = 0
+			let lagCounter = 0
+			let highs = []
+			let lows = []
+			let closes = []
+			let spanAs = []
+			let spanBs = []
 
+			let conversionPeriodLow, conversionPeriodHigh
+			let basePeriodLow, basePeriodHigh
+			let spanbPeriodLow, spanbPeriodHigh
 
-			let conversionPeriodLow, conversionPeriodHigh;
-			let basePeriodLow, basePeriodHigh;
-			let spanbPeriodLow, spanbPeriodHigh;
-
-			tick = yield;
+			tick = yield
 			
 			while (true) {
 				// Keep a list of lows/highs for the max period
-				highs.push(tick.high);
-				lows.push(tick.low);
+				highs.push(tick.high)
+				lows.push(tick.low)
 				// Keep a list of closes for the lagging span
-				closes.push(tick.close);
+				closes.push(tick.close)
 
 				if(periodCounter < period) {
 					periodCounter++
 				} else {
-					highs.shift();
-					lows.shift();
+					highs.shift()
+					lows.shift()
 
 					// Tenkan-sen (ConversionLine): (9-period high + 9-period low)/2))
-					conversionPeriodLow = lows.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.min(a,b) );
-					conversionPeriodHigh = highs.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.max(a,b) );
-					let conversionLine = (conversionPeriodHigh + conversionPeriodLow) /2;
+					conversionPeriodLow = lows.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.min(a,b) )
+					conversionPeriodHigh = highs.slice(-_this.params.conversionPeriod).reduce( (a,b) => Math.max(a,b) )
+					let conversionLine = (conversionPeriodHigh + conversionPeriodLow) /2
 
 					// Kijun-sen (Base Line): (26-period high + 26-period low)/2))
-					basePeriodLow = lows.slice(-_this.params.basePeriod).reduce( (a,b) => Math.min(a,b) );
-					basePeriodHigh = highs.slice(-_this.params.basePeriod).reduce( (a,b) => Math.max(a,b) );
-					let baseLine = (basePeriodHigh + basePeriodLow) /2;
+					basePeriodLow = lows.slice(-_this.params.basePeriod).reduce( (a,b) => Math.min(a,b) )
+					basePeriodHigh = highs.slice(-_this.params.basePeriod).reduce( (a,b) => Math.max(a,b) )
+					let baseLine = (basePeriodHigh + basePeriodLow) /2
 
 					// Senkou Span A (Leading Span A): (Conversion Line + Base Line)/2))
-					let spanA = 0;
-					spanAs.push((conversionLine + baseLine) /2);
+					let spanA = 0
+					spanAs.push((conversionLine + baseLine) /2)
 
 					// Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
-					let spanB = 0;
-					spanbPeriodLow = lows.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.min(a,b) );
-					spanbPeriodHigh = highs.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.max(a,b) );
-					spanBs.push((spanbPeriodHigh + spanbPeriodLow) /2);
+					let spanB = 0
+					spanbPeriodLow = lows.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.min(a,b) )
+					spanbPeriodHigh = highs.slice(-_this.params.spanPeriod).reduce( (a,b) => Math.max(a,b) )
+					spanBs.push((spanbPeriodHigh + spanbPeriodLow) /2)
 
 					// Senkou Span A / Senkou Span B offset by 26 periods
 					if(spanCounter < _this.params.displacement) {
 						spanCounter++
 					} else {
-						spanA = spanAs.shift();
-						spanB = spanBs.shift();
+						spanA = spanAs.shift()
+						spanB = spanBs.shift()
 					}
 
 					// Chikou Span (Lagging Span): Close plotted 26 days in the past
-					let close = 0;
+					let close = 0
                     if(lagCounter > _this.params.displacement) {
                         lagCounter++
                     } else {
-                        close = parseFloat(closes.shift());
+                        close = parseFloat(closes.shift())
                     }
 
 
@@ -100,9 +98,9 @@ class Ichimoku {
 		
 	}
 	nextValue(price) {
-		return this.generator.next(price).value;
+		return this.generator.next(price).value
 	}
 
 }
 
-module.exports = Ichimoku;
+module.exports = Ichimoku
